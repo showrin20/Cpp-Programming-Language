732eb773e8e0026aad2e4e10888b5235cc7e178a
Showrin Rahman <99120088+showrin20@users.noreply.github.com>
2023-01-01 23:58:24 +0600

    Create basic3.cpp

---
+++ basic3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n";
+  cout << "I am learning C++ \n";
+  cout << "Hi! \n\n";//two line gap
+  cout << "I am Showrin \n";
+  cout << "I Love" << endl;
+  cout << "learning C++\n";
+  cout << "C++ is a \t"; //same line with tab
+  cout << "interesting language\n";
+  cout << "Good choice for Competetive Programming\\";
+  cout << "game development";
+  return 0;
+}
