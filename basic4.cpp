7b7595adf263e369ce66ccdefc9f1cde45650a1e
Showrin Rahman <99120088+showrin20@users.noreply.github.com>
2023-01-02 00:24:14 +0600

    Create basic4.cpp

---
+++ basic4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    int z;
+    cout<<"Type x";
+    cin>>x;
+    cout<<"Type y";
+    cin>>y;
+    z=x+y;
+    cout<<"Sum is: "<<z;
+  return 0;
+}
