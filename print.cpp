b33c1ad13c31706052ed069cc74375ba0e470f21
Showrin Rahman <99120088+showrin20@users.noreply.github.com>
2023-01-01 23:41:44 +0600

    Delete print.cpp

--- print.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>//	It is used to define the cout, cin and cerr objects
-/*lls the compiler to use the std namespace. 
-Namespaces are a relatively recent addition to C++.*/
-using namespace std;
-//print
-int main () {
- cout<<"Hello World!";
-  return 0;
-}
