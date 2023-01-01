0198f2750a29e2b1fd9c9198076468ca3e3d46ea
Showrin Rahman <99120088+showrin20@users.noreply.github.com>
2023-01-01 23:44:24 +0600

    Create basic1.cpp

---
+++ basic1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>//	It is used to define the cout, cin and cerr objects
+/*lls the compiler to use the std namespace. 
+Namespaces are a relatively recent addition to C++.*/
+using namespace std;
+//print
+int main () {
+ cout<<"Hello World!";
+  return 0;
+}
